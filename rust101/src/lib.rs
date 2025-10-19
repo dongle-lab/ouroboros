b74f762c9bcff2ee6b88ba8bde5c7e806b401e35
pulppixel <hwankeee@naver.com>
2025-10-19 22:55:01 +0900

    update lib.rs

--- rust101/src/lib.rs
+++ rust101/src/lib.rs
@@ -1,8 +1,16 @@
 pub fn foo() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    let word = String::from("qwe_qwe qwe qwedas d");
+    first_word(&word);
+}
 
-    println!("{}", y);
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
